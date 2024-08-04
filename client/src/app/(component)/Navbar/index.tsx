a21b4129ef711927f8a6c684beba402184c4b6bf
amak nabil <amaknabil2001@gmail.com>
2024-08-04 17:39:47 +0800

    "second commit"

--- client/src/app/(component)/Navbar/index.tsx
+++ client/src/app/(component)/Navbar/index.tsx
@@ -3,15 +3,29 @@
 import React from 'react'
 import {Bell, Menu, Settings, Sun} from "lucide-react";
 import Link from 'next/link';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsSidebarCollapsed } from '@/state';
 
 const Navbar = () => {
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+    );
+
+    const  toggleSidebar = () =>{
+        dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
+
+    const sidebarClassname = `fixed flex-col' ${
+        isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+    } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
   return (
     <div className = "flex justify-between items-center w-full mb-7">
         {/* LEFT SIDE */}
         <div className = "flex justify-between items-centergap-5">
             <button 
                 className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-            onClick={() => {}}
+            onClick={toggleSidebar}
             >
             <Menu className = "w-4 h-4"/>
             </button>
