49560034655f46ff2cffab77c5c2830aa676b7bb
amak nabil <amaknabil2001@gmail.com>
2024-08-04 12:26:16 +0800

    first commit

---
+++ client/src/app/(component)/Sidebar/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { Menu } from 'lucide-react';
+import React from 'react';
+
+
+const Sidebar = () => {
+
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+    );
+
+    
+  return (
+    <div>
+        {/*TOP LOGO*/}
+        <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+            <div>Logo</div>
+            <h1 className="font-extrabold text-2xl">AN</h1>
+        
+        <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+         onClick={() =>{}}
+         >
+            <Menu className="w-4 h-4" />
+        </button>
+        </div>
+
+        {/*LINKS*/}
+        <div className="flex-grow mt-8">
+            {/* LINKS HERE */}
+        </div>
+
+        {/* Footer */}
+        <div>
+            <p className="text-center text-xs text-gray-500">&copy; 2024 AMAK</p>
+        </div>
+    </div>
+  );
+};
+
+export default Sidebar;
